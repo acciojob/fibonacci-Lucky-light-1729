046aec3f30cc1bfe071d18f719e45d9cb038329b
Lucky-light-1729 <agrawalanay20@gmail.com>
2025-05-31 09:37:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 function fibonacci(num) {
 // your code here
-	if(num==0)
-	{
-		return 0;
-	}
-	if(num==1)
-	{
-		return 1;
-	}
-	let a=0,b=1;
-	for (let i=2;i<=num;i++)
-	{
-		let temp = a + b;
-	    a = b;
-	    b = temp;
-	}
-	return b;
+	if (num <= 1) 
+		return num;
+	return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
